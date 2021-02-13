181eab7042f6001d589e2047a61cdf31eedd01b5
c-klee-run <kristy.allred@gmail.com>
2021-02-13 16:01:16 -0500

    updated employee_db.sql

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -20,3 +20,62 @@ e.emp_no=dm.emp_no
 JOIN departments AS d ON
 dm.dept_no=d.dept_no;
 
+--list the department of each employee with the following information: employee number, last name,
+--first name, **department name**
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+WHERE e.emp_no IN
+(
+	SELECT de.emp_no
+	FROM dept_emp AS de
+	WHERE de.dept_no IN
+	(
+		SELECT d.dept_no
+		FROM departments AS d
+		)
+);
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names 
+--begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE
+	first_name = 'Hercules'
+	AND last_name LIKE 'B%';
+	
+--List all employees in the Sales department, including their employee number, last name, first name, 
+--and **department name**. UNION?
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+WHERE e.emp_no IN
+(
+	SELECT de.emp_no
+	FROM dept_emp AS de
+	WHERE de.dept_no IN
+	(
+		SELECT d.dept_no
+		FROM departments AS d
+		WHERE d.dept_name = 'Sales')
+);
+
+--List all employees in the Sales and Development departments, including their employee number, 
+--last name, first name, and **department name**. UNION?
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+WHERE e.emp_no IN
+(
+	SELECT de.emp_no
+	FROM dept_emp AS de
+	WHERE de.dept_no IN
+	(
+		SELECT d.dept_no
+		FROM departments AS d
+		WHERE d.dept_name = 'Sales'
+		OR d.dept_name = 'Development')
+);
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees 
+--share each last name.
+SELECT last_name, COUNT(last_name) AS "last_name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY "last_name_count" DESC;
